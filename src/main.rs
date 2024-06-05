702d14b91e3b6093772e6fe19faf20dd8c01594b
cecile hirschauer <cecile.h.75@gmail.com>
2024-06-05 15:04:09 +0200

    Comparing guess number and secret number

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io; // prelude: bring io library into scope
+use std::cmp::Ordering; // prelude: bring Ordering enum into scope - cmp: comparison crate (dependency in Cargo.toml)
 use rand::Rng; // prelude: bring Rng trait into scope - rand: random number generator crate (dependency in Cargo.toml) Rng: random number generator
 
 fn main() {
@@ -19,5 +20,16 @@ fn main() {
         .read_line(&mut guess) // read_line method on the standard input handle to get input from the user. & indicates that this argument is a reference
         .expect("Failed to read line");
 
+    // introducing guess as a new variable with a new type: u32 (unsigned 32-bit integer)
+    // trim: method on the string to eliminate whitespace* (spaces, tabs, etc.) and newlines
+    // parse: method on strings that parses a string into some kind of number
+    // expect: method that allows us to handle potential failure by providing an error message
+    let guess: u32 = guess.trim().parse().expect("Please type a number!"); // shadowing: reusing the guess variable name
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
