021b99f4365bf2a335ffde35fbdbb588592b55cb
cecile hirschauer <cecile.h.75@gmail.com>
2024-06-05 14:28:19 +0200

    Generating a random number

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use std::io; // prelude: bring io library into scope
+use rand::Rng; // prelude: bring Rng trait into scope - rand: random number generator crate (dependency in Cargo.toml) Rng: random number generator
 
 fn main() {
     println!("Guess the number!");
+
+    // thread_rng: function that gives us the particular random number generator that we're going to use.
+    // gen_range: method that generates a random number between the two numbers we pass as arguments
+    // (inclusive on the lower bound but exclusive on the upper bound)
+    let secret_number = rand::thread_rng().gen_range(1..= 101);
+
+    println!("The secret number is: {}", secret_number);
 
     println!("Please input your guess.");
 
